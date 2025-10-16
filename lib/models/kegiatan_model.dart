efbd033a44f5fd1421d5a34424c8da5f372eb05c
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2025-10-16 21:58:36 +0700

    feat(danen_feature): Add Kegiatan - Tambah Page Feature

---
+++ lib/models/kegiatan_model.dart
@@ -0,0 +1,19 @@
+class Kegiatan {
+  final int id;
+  final String nama;
+  final String kategori;
+  final String tanggal;
+  final String lokasi;
+  final String penanggungJawab;
+  final String deskripsi;
+
+  Kegiatan({
+    required this.id,
+    required this.nama,
+    required this.kategori,
+    required this.tanggal,
+    required this.lokasi,
+    required this.penanggungJawab,
+    required this.deskripsi,
+  });
+}
