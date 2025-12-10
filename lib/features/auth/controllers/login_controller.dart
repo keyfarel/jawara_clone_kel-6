4f4cb17bdda8ab7c25c62ac0031d4089ea051810
farelsukabelajar <keyfarel645@gmail.com>
2025-12-10 03:49:05 +0000

    feat(auth): add auth face beometric

--- lib/features/auth/controllers/login_controller.dart
+++ lib/features/auth/controllers/login_controller.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import '../data/auth_repository.dart';
 import '../data/models/login_request.dart';
 
-class LoginController with ChangeNotifier {
+class LoginController extends ChangeNotifier {
   final AuthRepository repository;
-
-  bool isLoading = false;
 
   LoginController(this.repository);
 
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  // Login Biasa
   Future<Map<String, dynamic>> login(String email, String password) async {
-    isLoading = true;
+    _isLoading = true;
     notifyListeners();
 
-    final request = LoginRequest(email: email, password: password);
-    final result = await repository.login(request);
+    try {
+      final request = LoginRequest(email: email, password: password);
+      final result = await repository.login(request);
+      return result;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
 
-    isLoading = false;
+  // Login Wajah
+  Future<Map<String, dynamic>> loginFace(XFile selfie) async {
+    _isLoading = true;
     notifyListeners();
 
-    return result;
+    try {
+      // PERBAIKAN: Panggil langsung repository.loginFace (karena sudah kita buat methodnya di repo)
+      final result = await repository.loginFace(selfie); 
+      return result;
+    } catch (e) {
+        return {'status': 'error', 'message': e.toString()};
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
   }
 }
